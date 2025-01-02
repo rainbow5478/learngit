d40686e1191daba0f2b232200bfdb5118e61b911
firefly <firefly@firefly.test>
2025-01-02 16:28:25 +0800

    edit 5 by firefly again 2

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 edit 1 by firefly
+<<<<<<< HEAD
 eidt 2 by firefly
 edit 3 by partner
+=======
+edit 2 by firefly
+edit 4 by firefly
+>>>>>>> ac85556 (edit 4 by firefly)

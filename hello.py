0b20a5adb5b0f2779ac39fe90680ca40dd58dd58
firefly <firefly@firefly.test>
2025-01-03 09:59:45 +0800

    a 1111

--- hello.py
+++ hello.py
@@ -1,14 +1,3 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-edit 1 by firefly
-<<<<<<< HEAD
-eidt 2 by firefly
-edit 3 by partner
-=======
-edit 2 by firefly
-edit 4 by firefly
-<<<<<<< HEAD
->>>>>>> ac85556 (edit 4 by firefly)
-=======
-edit 5 by firefly
->>>>>>> b81fff1 (edit 5 by firefly)
+only one by firefly 2025-01-03 09:58:00
